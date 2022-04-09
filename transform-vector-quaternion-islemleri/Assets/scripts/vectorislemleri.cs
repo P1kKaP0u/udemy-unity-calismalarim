1fb162675b2c922bd0503dacfd6e7deace894292
Mustafa Kemal Aktaş <mustafaktaskemal@gmail.com>
2022-04-09 16:36:04 +0300

    Bazı vektör methodları incelendi

--- transform-vector-quaternion-islemleri/Assets/scripts/vectorislemleri.cs
+++ transform-vector-quaternion-islemleri/Assets/scripts/vectorislemleri.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class vectorislemleri : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Static Methodlar
+
+    public Transform hedef;
+    public Transform hedef2;
+
+    private float bas_zaman;
+
     void Start()
     {
-        
+        bas_zaman = Time.time;
+
+        /*
+            Time.time
+            Time.deltaTime
+            TÝme.timeScale
+        */
+
+
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+
+
+        // learp iki vektör arasýnda doðrusal bir çizgi oluþturulmasýný saðlar
+        transform.position = Vector3.Lerp(hedef.position, hedef2.position, (Time.time - bas_zaman) * .2f);
+         
+
+
+        // distance iki vektör arasýndaki mesafeyi döndürür
+        /*
+        float fark = Vector3.Distance(hedef.position,transform.position);
+
+        Debug.Log("Aradaki Mesafe: " + fark);
+        */
+
+
+
+        // angel iki vektör arasýndaki farký derece cinsinden verir
+        /*
+        Vector3 mesafe = hedef.position - transform.position;
+
+        float angle = Vector3.Angle(mesafe, transform.up);
+
+        Debug.Log(angle);
+
+        if (angle < 5f)
+        {
+            Debug.Log("Açý Saðlandý");
+        }
+        */
+
     }
 }
